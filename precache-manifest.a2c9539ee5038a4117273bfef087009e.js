a07dadca7ad04d9cfe109ac91bc2f5b0fb2f6b89
Travis Eden <i@traviseden.com>
2019-02-14 14:27:19 -0800

    Updates

---
+++ precache-manifest.a2c9539ee5038a4117273bfef087009e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2fa74a96f8bf520c4ef",
+    "url": "/oak-bike-client/static/css/main.94e1b850.chunk.css"
+  },
+  {
+    "revision": "d2fa74a96f8bf520c4ef",
+    "url": "/oak-bike-client/static/js/main.d2fa74a9.chunk.js"
+  },
+  {
+    "revision": "1d1c4b8dc840b200bd90",
+    "url": "/oak-bike-client/static/js/runtime~main.1d1c4b8d.js"
+  },
+  {
+    "revision": "2ee6c9c270e27653222c",
+    "url": "/oak-bike-client/static/js/2.2ee6c9c2.chunk.js"
+  },
+  {
+    "revision": "2b1346470a56198d1ca1e6c13b186197",
+    "url": "/oak-bike-client/index.html"
+  }
+];
