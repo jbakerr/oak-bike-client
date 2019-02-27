ed9f273d47ef5d8e15fb1333e7bc40c62598b3d7
Travis Eden <i@traviseden.com>
2019-02-26 19:22:54 -0800

    latest

--- src/components/ReportObstruction/Location.jsx
+++ src/components/ReportObstruction/Location.jsx
@@ -4,23 +4,26 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import { DETAILS } from "../../constants/report";
 import { getUserLocation } from "../../utils/report";
 
+const mapBottomOffset = 60;
+
 const Location = ({initCenter, initZoom, setLocation, setStatus}) => {
-  const [height, setHeight] = useState(400);
+  // <Map> requires an absolute height
+  const [height, setHeight] = useState(document.documentElement.clientHeight - mapBottomOffset);
   useEffect(() => {
     const handleResize = () => {
-      setHeight(window.innerHeight);
+      setHeight(document.documentElement.clientHeight - mapBottomOffset);
     }
     window.addEventListener("resize", handleResize);
-    setTimeout(handleResize, 0);
+    handleResize();
     return () => {
       window.removeEventListener("resize", handleResize);
     }
   }, []);
 
   const [center, setCenter] = useState(initCenter);
-  const [zoom, setZoom] = useState(initZoom);
-  const onDrag = (event) => setCenter(event.target.getCenter());
-  const onZoom = (event) => setZoom(event.target.getZoom());
+  const onDrag = (event) => {
+    setCenter(event.target.getCenter());
+  }
   useEffect(() => {
     getUserLocation()
       .then(location => {
@@ -31,6 +34,11 @@ const Location = ({initCenter, initZoom, setLocation, setStatus}) => {
         console.warn("Could not get user location");
       });
   }, []);
+
+  const [zoom, setZoom] = useState(initZoom);
+  const onZoom = (event) => {
+    setZoom(event.target.getZoom());
+  }
 
   const saveLocation = () => {
     setLocation(center);
@@ -44,7 +52,7 @@ const Location = ({initCenter, initZoom, setLocation, setStatus}) => {
         zoom={zoom}
         onDrag={onDrag}
         onZoom={onZoom}
-        style={{ height: height }}
+        style={{ height }}
       >
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -55,8 +63,8 @@ const Location = ({initCenter, initZoom, setLocation, setStatus}) => {
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
-        <button onClick={saveLocation}>Save Location</button>
       </Map>
+      <button onClick={saveLocation}>Save Location</button>
     </>
   );
 };
--- src/components/ReportObstruction/Upload.jsx
+++ src/components/ReportObstruction/Upload.jsx
@@ -4,9 +4,21 @@ import { LOCATION } from "../../constants/report";
 
 // import { fakeImageUpload } from "../../utils/report";
 
+// import s3 from "aws-sdk/clients/s3";
+import S3 from "aws-s3";
+const s3Config = {
+  bucketName: "oak-bike",
+  region: "us-west-1",
+};
+const s3Client = new S3(s3Config);
+
 const Upload = ({ setUpload, setStatus }) => {
   const onDrop = async (files) => {
-    console.log("uploading:", files[0]);
+    const file = files[0];
+    await s3Client.uploadFile(file)
+      .then(data => { console.log("data:", data); })
+      .catch(err => { console.log("err:", err); });
+    console.log("this");
     setUpload(files[0]);
     setStatus(LOCATION);
   }
